6bff37616d140722191394feaf5c6eb7a717f2b0
Will Downey <willd20@vt.edu>
2022-07-06 21:20:16 -0400

    Add users route

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import usersRoutes from './routes/users.js';
 
 const app = express();
 const PORT = 5000;
 
 // JSON data format
 app.use(bodyParser.json());
+
+app.use('/users', usersRoutes);
+
+// test route
+app.get('/', (req, res) => {
+    res.send('Hello from Homepage.');
+});
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
